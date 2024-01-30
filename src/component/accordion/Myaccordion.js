82bebfb2b6ac523f93f563a649908f4677413a1f
vishu bhandari <deevishu1234@gmail.com>
2024-01-30 12:25:26 +0530

    c

--- src/component/accordion/Myaccordion.js
+++ src/component/accordion/Myaccordion.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function Myaccordion({question,answer}) {
+function Myaccordion({ question, answer }) {
+  const [show, setShow] = useState(false);
 
-    const [show,setShow]=useState(false );
+  const click = () => {
+    setShow(true);
+  };
+
   return (
     <>
-        <div className="main-heading">
-            <p onClick={()=>setShow(!show)}>{ show ? "➖" : "➕" } </p> 
-           
-            <h3>{question}</h3>
-        </div>
-        {
-            show &&  <p className='answers'>{answer}</p>
-        }
-       
+      <div className="main-heading">
+        <p onClick={() => setShow(!show)}>{show ? "➖" : "➕"} </p>
+        <p onClick={click}></p>
+        <h3>{question}</h3>
+      </div>
+      {show && <p className="answers">{answer}</p>}
     </>
-  )
+  );
 }
 
-export default Myaccordion
+export default Myaccordion;
